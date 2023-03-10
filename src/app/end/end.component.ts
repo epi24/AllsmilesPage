96a088df7ca8071c42b2992578fc2d6633f74da8
epi24 <epilaws2002@yahoo.de>
2023-03-11 00:39:52 +0100

    Alle Bilder sind auf der Seite Navbar is überall

--- src/app/end/end.component.ts
+++ src/app/end/end.component.ts
@@ -8,8 +8,31 @@ import { Component } from '@angular/core';
 export class ENDComponent {
   listOfPictures:string[]=[
     "../../assets/imagesEND/1.jpeg",
-    "../../assets/imagesEND/2.jpeg",
-
+    "../../assets/imagesEND/2.jfif",
+    "../../assets/imagesEND/3.jpeg",
+    "../../assets/imagesEND/4.jpeg",
+    "../../assets/imagesEND/5.jpeg",
+    "../../assets/imagesEND/6.jfif",
+    "../../assets/imagesEND/7.jpeg",
+    "../../assets/imagesEND/8.jpeg",
+    "../../assets/imagesEND/9.jfif",
+    "../../assets/imagesEND/10.jfif",
+    "../../assets/imagesEND/11.jfif",
+    "../../assets/imagesEND/12.jpeg",
+    "../../assets/imagesEND/13.jpeg",
+    "../../assets/imagesEND/END-Let_the_feeling_of_peace.jpeg",
+    "../../assets/imagesEND/END-love.jpeg",
+    "../../assets/imagesEND/END-prayer.jpeg",
+    "../../assets/imagesEND/END-the_world.jpeg",
+    "../../assets/imagesEND/END-therapy.jpeg",
+    "../../assets/imagesEND/END-to_be_celebrated.jpeg",
+    "../../assets/imagesEND/END-to_be_happy.jpeg",
+    "../../assets/imagesEND/END-to_be_healthy.jpeg",
+    "../../assets/imagesEND/END-to_be_well_balanced.jpeg",
+    "../../assets/imagesEND/END-to_dance.jpeg",
+    "../../assets/imagesEND/END-to_heal.jpeg",
+    "../../assets/imagesEND/END-to_know_their_worth.jpeg",
+    "../../assets/imagesEND/END-to_relax.jpeg",
 
   ]
   picEnd1:string = "assets/imagesEND/1.jpeg";
